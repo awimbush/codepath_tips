db6dc08e320f007a9389b0cfe902ceecae34862b
Alex Wimbush <awimbush@climate.com>
2015-09-13 21:06:58 -0700

    add random tip amount on shake

--- tips/ViewController.swift
+++ tips/ViewController.swift
@@ -14,36 +14,70 @@ class ViewController: UIViewController {
     @IBOutlet weak var billField: UITextField!
     @IBOutlet weak var totalLabel: UILabel!
     @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var surpriseButton: UIButton!
+    
+    let tipPercentages = [0.18, 0.2, 0.25]
+    var tipPercentage: Double = 0
+    var billAmount: Double = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
         tipLabel.text = "$0.00"
         totalLabel.text = "$0.00"
-        
-        
     }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
 
+    @IBAction func onClick(sender: AnyObject) {
+        surprise()
+    }
+    
+    
     @IBAction func onEditingChanged(sender: AnyObject) {
-        var tipPercentages = [0.18, 0.2, 0.25]
-        var tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
-        var billAmount = NSString(string: billField.text!).doubleValue
-        var tip = billAmount * tipPercentage
-        var total = billAmount + tip
-        
-        tipLabel.text = String(format: "$%.2f", tip)
-        totalLabel.text = String(format: "$%.2f", total)
-        
+        setBillAmount()
+        if tipControl.selectedSegmentIndex != -1 {
+            tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
+        }
+        updateAndDisplayTotal()
     }
 
     @IBAction func onTap(sender: AnyObject) {
         view.endEditing(true)
     }
+    
+    override func motionEnded(motion: UIEventSubtype, withEvent event: UIEvent) {
+        if motion == .MotionShake {
+            surprise()
+        }
+    }
+    
+    func generateRandomTipPercent() -> Double {
+        return Double(arc4random_uniform(101))/100
+    }
+    
+    func updateAndDisplayTotal() {
+        var tip = billAmount * tipPercentage
+        var total = billAmount + tip
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
+    
+    func setBillAmount() {
+        billAmount = NSString(string: billField.text!).doubleValue
+    }
+    
+    func surprise() {
+        tipControl.selectedSegmentIndex = -1
+        setBillAmount()
+        tipPercentage = generateRandomTipPercent()
+        updateAndDisplayTotal()
+    }
+    
 }
 
